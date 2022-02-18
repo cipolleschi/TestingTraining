bdbb4a97885079fbfbee0f6be3ace9949db5822b
Andras Pal <andras.pal@outlook.com>
2022-02-18 23:18:12 +0000

    add tests for the add function

--- Chapter1/Tests/CalculatorTests/AddTests.swift
+++ Chapter1/Tests/CalculatorTests/AddTests.swift
@@ -2,38 +2,55 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
-    func testAdd_whenOnePlusOne_returnsTwo() throws {
-        // Given: This block is used to prepare the data for the test. Sometimes it is also
-        // called the Arrange step.
-        let first = 1
-        let second = 1
-        
-        // When: This block is used to perform the operation to test. Sometimes it is also called
-        // the Act step.
-        let result = Calculator.add(first, second)
-        
-        
-        // Then: This block is used to check that the output is what we expect. Sometimes it is also
-        // called the Assert step.
-        XCTAssertEqual(result, 2)
-    }
+  func testAdd_whenOnePlusOne_returnsTwo() throws {
+    let first = 1
+    let second = 1
     
-    func testAdd_whenFirstIsZero_returnsSecond() throws {
-        // neutral element: 0 + x = x
-        
-    }
+    let result = Calculator.add(first, second)
     
-    func testAdd_whenSecondIsZero_returnsFirst() throws {
-        // neutral element: x + 0 = x
-        
-    }
+    XCTAssertEqual(result, 2)
+  }
+  
+  func testAdd_whenFirstIsZero_returnsSecond() throws {
+    // neutral element: 0 + x = x
+    let first = 0
+    let second = 5
     
-    func testAdd_twoPlusThreeAndThreePlusTwo_returnsTheSameResult() throws {
-        // commutative property: a + b = b + a
-        
-    }
+    let result = Calculator.add(first, second)
     
-    func testAdd_twoPlusThree_PlusFour_AndTwoPlus_ThreePlusFour_returnsTheSameResult() throws {
-        // associative property: (a+b)+c = a+(b+c)
-    }
+    XCTAssertEqual(result, 5)
+  }
+  
+  func testAdd_whenSecondIsZero_returnsFirst() throws {
+    // neutral element: x + 0 = x
+    let first = 3
+    let second = 0
+    
+    let result = Calculator.add(first, second)
+    
+    XCTAssertEqual(result, 3)
+  }
+  
+  func testAdd_twoPlusThreeAndThreePlusTwo_returnsTheSameResult() throws {
+    // commutative property: a + b = b + a
+    let first = 4
+    let second = 6
+    
+    let result1 = Calculator.add(first, second)
+    let result2 = Calculator.add(second, first)
+    
+    XCTAssertEqual(result1, result2)
+  }
+  
+  func testAdd_twoPlusThree_PlusFour_AndTwoPlus_ThreePlusFour_returnsTheSameResult() throws {
+    // associative property: (a+b)+c = a+(b+c)
+    let first = 2
+    let second = 3
+    let third = 4
+    
+    let result1 = Calculator.add(Calculator.add(first, second), third)
+    let result2 = Calculator.add(first, Calculator.add(second, third))
+    
+    XCTAssertEqual(result1, result2)
+  }
 }
